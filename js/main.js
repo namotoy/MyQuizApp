499dd24d64163c4f2011d7837d68432734109cb6
namotoy <namotopet0615@gmail.com>
2022-06-15 13:07:04 +0900

    クイズデータのセット

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+{
+  const question = document.getElementById('question')
+  const choices = document.getElementById('choices')
+  const btn = document.getElementById('btn')
+
+  const quizSet = [
+{q: 'What is A?', c:[ 'A0','A1','A2','A3']},
+{q: 'What is B?', c:[ 'B0','B1','B2','B3']},
+{q: 'What is C?', c:[ 'C0','C1','C2','C3']},
+  ]
+let currentNum = 0;
+}
