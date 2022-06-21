1d67f14ab4a4623959edb159b425672ea82a91e0
namotoy <namotopet0615@gmail.com>
2022-06-21 14:51:14 +0900

    JSで選択肢表示

--- js/main.js
+++ js/main.js
@@ -10,4 +10,12 @@
 {q: 'What is C?', c:[ 'C0','C1','C2','C3']},
   ]
 let currentNum = 0;
+
+question.textContent = quizSet[currentNum].q;
+
+quizSet[currentNum].c.forEach(choice => {
+ const li = document.createElement('li');
+ li.textContent = choice;
+ choices.appendChild(li);
+});
 }
